81308bfdde63e2209c1117d858f6245bf721ac2c
joyroy-bd <68392629+joyroy-bd@users.noreply.github.com>
2020-11-09 01:46:55 +0600

    Add files via upload

---
+++ 1.First Program.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main(){
+    printf("My name is Joy; \n");
+    printf("Class : 11;\n");
+    printf("Phone : 01936033735; \n");
+}
---
+++ 2.Variable&Datatype.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int in;
+	float fl;
+	double dob;
+	char ch;
+	
+	in=6;
+	fl=7.8;
+	dob=5.333;
+	ch ='J';
+	
+	printf("Integar : %i\n",in);
+	printf("Float : %.5f\n",fl);
+	printf("Double : %.5lf\n",dob);
+	printf("Charecter : %c\n",ch);
+}
